3932f0fd384d05b2a0862ee7ed3111632d24a33c
Honeybone <39705149+Honeybone@users.noreply.github.com>
2018-05-28 18:08:09 +0100

    Update Exercise1-19.cpp

--- Exercise1-19.cpp
+++ Exercise1-19.cpp
@@ -7,19 +7,20 @@ int main()
     int a = 0;
     int b = 0;
 
-    cout << "Please enter two numbers." << endl;
+    cout << "Please enter two numbers.\n" << endl;
 
     cin >> a >> b;
 
     cout << "The numbers you have entered are " << a << " and " << b << ".\n" << endl;
 
     if (a<b){ 
-        cout <<"Number " <<a << " is smaller than number " <<b << endl;
+        cout <<"The numbers between " << a << " and " << b << " are: \n" << endl;
         while (a<=b){
             cout << a <<endl; ++a;
         }
         }
-        else { cout << "Number " <<a<<" is bigger than number " <<b<<endl;
+        else { 
+            cout << "The numbers between " << b <<" and " << a << " are: \n" <<endl;
         while (a>=b){
             cout << b <<endl; ++b;
         }
